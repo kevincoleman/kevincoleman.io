3c9950d25c216f76d15f1eaee2738035dfa4fbe3
kevincoleman <kevin@kevincoleman.io>
2020-07-05 15:55:50 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -19,12 +19,12 @@ const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  'index.html',
-  './', // Alias for index.html
-  './css/main.css',
-  './css/home.css',
-  './css/projects.css',
-  './projects/'
+  // 'index.html',
+  // './', // Alias for index.html
+  // './css/main.css',
+  // './css/home.css',
+  // './css/projects.css',
+  // './projects/'
 ];
 
 // The install handler takes care of precaching the resources we always need.
