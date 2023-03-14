5649112fb7e89856a6d622dd6c9cca82aea56ab6
Kevin Coleman <github@kevincoleman.io>
2023-03-14 01:44:14 -0700

    add CTA for adding as a PWA

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,13 @@
+function iOS() {
+    return /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
+  }
+
 function isInstalled() {
     // For iOS
     if(window.navigator.standalone) return true
   
     // For Android
-    if(window.matchMedia('(display-mode: standalone)').matches) return true
+    // if(window.matchMedia('(display-mode: standalone)').matches) return true
   
     // If neither is true, it's not installed
     return false
