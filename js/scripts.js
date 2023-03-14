3fd26b75f365b50651778980c5e844c2b65fa42f
Kevin Coleman <github@kevincoleman.io>
2023-03-14 01:50:09 -0700

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
+function iOS() {
+    return [
+      'iPad Simulator',
+      'iPhone Simulator',
+      'iPod Simulator',
+      'iPad',
+      'iPhone',
+      'iPod'
+    ].includes(navigator.platform)
+    // iPad on iOS 13 detection
+    || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+  }
+
 function isInstalled() {
     // For iOS
     if(window.navigator.standalone) return true
   
     // For Android
-    if(window.matchMedia('(display-mode: standalone)').matches) return true
+    // if(window.matchMedia('(display-mode: standalone)').matches) return true
   
     // If neither is true, it's not installed
     return false
