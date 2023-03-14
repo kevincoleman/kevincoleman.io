fe7a1225762fa517569ea815f99bb3c2df5e294e
Kevin Coleman <github@kevincoleman.io>
2023-03-14 00:27:51 -0700

    try out pwa detection script

---
+++ src/js/scripts.js
@@ -0,0 +1,10 @@
+function isInstalled() {
+    // For iOS
+    if(window.navigator.standalone) return true
+  
+    // For Android
+    if(window.matchMedia('(display-mode: standalone)').matches) return true
+  
+    // If neither is true, it's not installed
+    return false
+}
